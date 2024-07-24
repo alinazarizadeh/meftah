0e839719237153ae125f405ff5708a4bfd5535f0
Ali Nazarizadeh <52278732+alinazarizadeh@users.noreply.github.com>
2024-07-24 13:34:25 +0330

    Add files via upload

---
+++ untitled3.py
@@ -0,0 +1,116 @@
+import time
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import streamlit as st
+import datetime
+import numpy as np
+import openpyxl
+
+
+st. set_page_config(layout="wide")
+
+
+col100, col200, col300, col400, col500, col600, col700 = st.columns(7)
+
+with col100:
+    pass
+with col200:
+    pass
+with col300:
+    pass
+with col400:
+    TEST = st.text_input("رمز ورود",type="password")
+with col500:
+    pass
+with col600:
+    pass
+with col700:
+    pass
+
+if TEST == "123456" or TEST == "0000" or TEST == "1111":
+    if TEST == "123456":
+        st.write("آقای علی نظری زاده خوش آمدید")
+    elif TEST == "0000":
+        st.write("کارشناس فروش خوش آمدید")
+    elif TEST == "1111":
+        st.write("واحد برنامه ریزی خوش آمدید")
+    
+
+
+ 
+        
+    df_etender_Estealam_0427 = pd.read_excel("D:\Data Analysis\Dataset\date_0427\etender_Estealam_0427.xlsx")
+    df_etender_Monagheseh_0427 = pd.read_excel("D:\Data Analysis\Dataset\date_0427\etender_Monagheseh_0427.xlsx")
+    df_hezarehinfo_Estealam_0427 = pd.read_excel("D:\Data Analysis\Dataset\date_0427\hezarehinfo_Estealam_0427.xlsx")
+    df_hezarehinfo_Monagheseh_0427 = pd.read_excel("D:\Data Analysis\Dataset\date_0427\hezarehinfo_Monagheseh_0427.xlsx")
+        
+
+
+    st.title("سامانه جمع آوری مناقصات شرکت مفتاح رایانه افزار",)
+    s = st.radio("انتخاب کنید", ["استعلام بها ایتندر" , "استعلام بها هزاره" , "مناقصه ایتندر" , "مناقصه هزاره"])
+        
+        
+    if s == "استعلام بها ایتندر":
+            
+        st.dataframe(df_etender_Estealam_0427)
+        df2 = df_etender_Estealam_0427['استان'].value_counts(sort=False)
+            
+    elif s== "استعلام بها هزاره":
+        st.dataframe(df_hezarehinfo_Estealam_0427)
+        df2 = df_hezarehinfo_Estealam_0427['استان'].value_counts(sort=False)
+            
+    elif s== "مناقصه ایتندر":
+        st.dataframe(df_etender_Monagheseh_0427)
+        df2 = df_etender_Monagheseh_0427['استان'].value_counts(sort=False)
+            
+    elif s== "مناقصه هزاره":
+        st.dataframe(df_hezarehinfo_Monagheseh_0427)
+        df2 = df_hezarehinfo_Monagheseh_0427['استان'].value_counts(sort=False)
+            
+    else:
+        pass
+        
+        
+    title2 = "-1"
+    option = "کد آگهی"
+    Columns = list(df_etender_Estealam_0427.columns.unique())
+    col10, col20 = st.columns(2)
+    with col10:
+        option = st.selectbox(
+        "فیلد قابل جستجو را وارد کنید",
+        (Columns))
+
+        #st.write("You selected:", option)
+        
+    with col20:
+        title2 = st.text_input("متن جستجو", )
+        
+    mask_to = df_etender_Estealam_0427[option].str.contains(title2)
+    v = df_etender_Estealam_0427[mask_to]
+    st.dataframe(v)
+        
+    st.write("\n")
+    st.title("گزارشات",)
+    st.bar_chart(df2)
+        
+    
+
+        
+        
+            
+            
+        
+        
+        
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
