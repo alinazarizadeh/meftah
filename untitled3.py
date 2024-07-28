108c1108bfa834293ae14499117791b82c1566eb
Ali Nazarizadeh <52278732+alinazarizadeh@users.noreply.github.com>
2024-07-28 10:01:12 +0330

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,16 +1,9 @@
-
 import pandas as pd
-
 import streamlit as st
+import openpyxl
 
-import numpy as np
-
-
-st. set_page_config(layout="wide")
-
-
+st. set_page_config(layout="wide", page_title='مفتاح رایانه افزار')
 col100, col200, col300, col400, col500, col600, col700 = st.columns(7)
-
 with col100:
     pass
 with col200:
@@ -18,7 +11,7 @@ with col200:
 with col300:
     pass
 with col400:
-    TEST = st.text_input("رمز ورود",type="password")
+    TEST = st.text_input("رمز ورود : 123456",type="password")
 with col500:
     pass
 with col600:
@@ -34,21 +27,23 @@ if TEST == "123456" or TEST == "0000" or TEST == "1111":
     elif TEST == "1111":
         st.write("واحد برنامه ریزی خوش آمدید")
     
-
-
- 
+    # import data
+    df_etender_Estealam_0427 = pd.read_excel("D:\Data Analysis\Dataset\date_0427\etender_Estealam_0427.xlsx")
+    df_etender_Monagheseh_0427 = pd.read_excel("D:\Data Analysis\Dataset\date_0427\etender_Monagheseh_0427.xlsx")
+    df_hezarehinfo_Estealam_0427 = pd.read_excel("D:\Data Analysis\Dataset\date_0427\hezarehinfo_Estealam_0427.xlsx")
+    df_hezarehinfo_Monagheseh_0427 = pd.read_excel("D:\Data Analysis\Dataset\date_0427\hezarehinfo_Monagheseh_0427.xlsx")
         
-    df_etender_Estealam_0427 = pd.read_excel("etender_Estealam_0427.xlsx")
-    df_etender_Monagheseh_0427 = pd.read_excel("etender_Monagheseh_0427.xlsx")
-    df_hezarehinfo_Estealam_0427 = pd.read_excel("hezarehinfo_Estealam_0427.xlsx")
-    df_hezarehinfo_Monagheseh_0427 = pd.read_excel("hezarehinfo_Monagheseh_0427.xlsx")
-        
+    # header
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        pass
+    with col2:
+        st.title("سامانه جمع آوری مناقصات شرکت مفتاح رایانه افزار",)
+    with col3:
+        pass
 
-
-    st.title("سامانه جمع آوری مناقصات شرکت مفتاح رایانه افزار",)
-    s = st.radio("انتخاب کنید", ["استعلام بها ایتندر" , "استعلام بها هزاره" , "مناقصه ایتندر" , "مناقصه هزاره"])
-        
-        
+    
+    s = st.radio("انتخاب کنید", ["استعلام بها ایتندر" , "استعلام بها هزاره" , "مناقصه ایتندر" , "مناقصه هزاره"], )   
     if s == "استعلام بها ایتندر":
             
         st.dataframe(df_etender_Estealam_0427)
@@ -69,39 +64,90 @@ if TEST == "123456" or TEST == "0000" or TEST == "1111":
     else:
         pass
         
-        
-    title2 = "-1"
-    option = "کد آگهی"
     Columns = list(df_etender_Estealam_0427.columns.unique())
     col10, col20 = st.columns(2)
-    with col10:
-        option = st.selectbox(
-        "فیلد قابل جستجو را وارد کنید",
-        (Columns))
+    
+    # Search by each column in Dataframe
+    if s == "استعلام بها ایتندر":
+        try:
+            Columns = list(df_etender_Estealam_0427.columns.unique())
+            with col10:
+                option = st.selectbox(
+                "فیلد قابل جستجو را وارد کنید",
+                (Columns))
+        
+            with col20:
+                title2 = st.text_input("متن جستجو", )
+            mask_to = df_etender_Estealam_0427[option].str.contains(title2)
+            v = df_etender_Estealam_0427[mask_to]
+            st.dataframe(v)
+        except:
+            pass
+                
+        
+    elif s== "استعلام بها هزاره":
+        try:
+            Columns = list(df_hezarehinfo_Estealam_0427.columns.unique())
+            with col10:
+                option = st.selectbox(
+                "فیلد قابل جستجو را وارد کنید",
+                (Columns))
+        
+            with col20:
+                title2 = st.text_input("متن جستجو", )
+            mask_to = df_hezarehinfo_Estealam_0427[option].str.contains(title2)
+            v = df_hezarehinfo_Estealam_0427[mask_to]
+            st.dataframe(v)
+        except:
+            pass
+           
+        
+    elif s== "مناقصه ایتندر":
+        try:
+            Columns = list(df_etender_Monagheseh_0427.columns.unique())
+            with col10:
+                option = st.selectbox(
+                "فیلد قابل جستجو را وارد کنید",
+                (Columns))
+        
+            with col20:
+                title2 = st.text_input("متن جستجو", )
+            mask_to = df_etender_Monagheseh_0427[option].str.contains(title2)
+            v = df_etender_Monagheseh_0427[mask_to]
+            st.dataframe(v)
+        except:
+            pass
+            
+        
+    elif s== "مناقصه هزاره":
+        try:
+            Columns = list(df_hezarehinfo_Monagheseh_0427.columns.unique())
+            with col10:
+                option = st.selectbox(
+                "فیلد قابل جستجو را وارد کنید",
+                (Columns))
+        
+            with col20:
+                title2 = st.text_input("متن جستجو", )
+            mask_to = df_hezarehinfo_Monagheseh_0427[option].str.contains(title2)
+            v = df_hezarehinfo_Monagheseh_0427[mask_to]
+            st.dataframe(v)
+        except:
+            pass
+            
+    else:
+        pass
 
-        #st.write("You selected:", option)
-        
-    with col20:
-        title2 = st.text_input("متن جستجو", )
-        
-    mask_to = df_etender_Estealam_0427[option].str.contains(title2)
-    v = df_etender_Estealam_0427[mask_to]
-    st.dataframe(v)
-        
     st.write("\n")
     st.title("گزارشات",)
     st.bar_chart(df2)
         
-    prompt = st.chat_input("Say something")
-    if prompt:
-        st.write(f"User has sent the following prompt: {prompt}")
-
+        
+        
         
         
             
             
-        
-        
         
         
         
